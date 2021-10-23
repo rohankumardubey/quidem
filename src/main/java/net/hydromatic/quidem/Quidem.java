cc0b60a1dea0e5c0b854b517ed69cbc292142265
NobiGo <nobigogle@gmail.com>
2021-10-23 16:46:25 +0800

    [CALCITE-4131] code format

--- src/main/java/net/hydromatic/quidem/Quidem.java
+++ src/main/java/net/hydromatic/quidem/Quidem.java
@@ -324,9 +324,8 @@ public class Quidem {
           resultSetException = null;
           resultSet = statement.executeQuery(sqlCommand.sql);
           if (resultSet != null) {
-            final OutputFormat format =
-                    (OutputFormat)
-                            env.apply(Property.OUTPUTFORMAT.propertyName());
+            final OutputFormat format = (OutputFormat) env
+                    .apply(Property.OUTPUTFORMAT.propertyName());
             format.format(resultSet, headerLines, bodyLines, footerLines,
                     sqlCommand.sort);
           }
